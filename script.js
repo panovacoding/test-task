bf1ce443f0e05f900026a82e0be2b525be798f7e
Lidiya <lidakonstantinova@mail.ru>
2023-07-29 17:28:40 +0400

    commit

--- script.js
+++ script.js
@@ -3,4 +3,20 @@ const menu = document.querySelector('.burger-menu');
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('burger-menu--open');
+});
+
+const swiper = new Swiper('.swiper', {
+    breakpoints: {
+        320: {
+            slidesPerView: 1.2,
+            spaceBetween: 16,
+        },
+        769: {
+            slidesPerView: 4,
+            navigation: {
+                nextEl: '.btn-swiper-next',
+                prevEl: '.btn-swiper-prev',
+              },
+        },
+    }
 })
